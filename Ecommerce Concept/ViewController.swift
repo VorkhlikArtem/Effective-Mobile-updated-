419c7eea4f0c8db7e1abe8653036d8caffe04656
artem <artemvorkhlik@mail.ru>
2022-12-03 15:11:07 +0300

    Initial Commit

---
+++ Ecommerce Concept/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ecommerce Concept
+//
+//  Created by Артём on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
